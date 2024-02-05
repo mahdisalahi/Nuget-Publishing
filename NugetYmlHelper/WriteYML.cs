62f9845c211fd7b7d7165e66e139c925d6b55206
Mahdi Salahinia <salahimahdi938@gmail.com>
2024-02-05 18:08:49 +0330

    AddWriteMethod

---
+++ NugetYmlHelper/WriteYML.cs
@@ -0,0 +1,10 @@
+﻿namespace NugetYmlHelper;
+
+public class WriteYML
+{
+    public void ExecuteCurrentConcept()
+    {
+        //Logic
+        Console.WriteLine("The correct concept has been created, please edit according to your needs...\r\n");
+    }
+}
